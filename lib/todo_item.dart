d829ca84685c373deb48e328beb1c2eaa34110d6
BankKeo <77560510+BankKeo@users.noreply.github.com>
2022-07-11 11:41:07 +0700

    add project

---
+++ lib/todo_item.dart
@@ -0,0 +1,16 @@
+class ToDoItem {
+  late String string;
+  late bool done;
+
+  ToDoItem(this.string) {
+    done = false;
+  }
+
+  ToDoItem.fromMap(Map map)
+      : string = map['string'],
+        done = map['done'];
+
+  Map toMap() {
+    return {'string': string, 'done': done};
+  }
+}
